193940a0d328576771cd71713d83c93ad8dfeccd
Quinn Vaughn <qvaughn3@gmail.com>
2021-11-05 18:49:54 -0500

    Added column slice.

---
+++ src/redux/slices/column.ts
@@ -0,0 +1,62 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { v4 as uuidv4 } from "uuid"
+
+type ColumnState = {
+  columns: Column[]
+}
+
+type Column = {
+  id: string
+  name: string
+}
+
+const initialState: ColumnState = {
+  columns: [],
+}
+
+export const columnSlice = createSlice({
+  name: "columns",
+  initialState,
+  reducers: {
+    addColumn: (state) => {
+      const column: Column = {
+        id: uuidv4(),
+        name: "Add a name",
+      }
+      state.columns.push(column)
+    },
+    deleteColumn: (state, action: PayloadAction<string>) => {
+      const index = state.columns.findIndex((col) => col.id === action.payload)
+
+      if (index === -1) return
+
+      state.columns.splice(index, 1)
+    },
+    editColumnName: (
+      state,
+      action: PayloadAction<{ name: string; id: string }>
+    ) => {
+      const column = state.columns.find((col) => col.id === action.payload.id)
+
+      if (!column) return
+
+      column.name = action.payload.name
+    },
+    moveColumn: (
+      state,
+      action: PayloadAction<{ id: string; newPosition: number }>
+    ) => {
+      const { newPosition, id } = action.payload
+      if (newPosition < 0 || newPosition > state.columns.length) return
+
+      const oldPosition = state.columns.findIndex((col) => col.id === id)
+
+      if (oldPosition === -1) return
+
+      const column = state.columns[oldPosition]
+
+      state.columns.splice(oldPosition, 1)
+      state.columns.splice(newPosition, 0, column)
+    },
+  },
+})
